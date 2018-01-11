b18b8ba9528a9fb387a2bc6bca8ac9c138932132
luyuanyuan <694436921@qq.com>
2018-01-12 00:22:06 +0800

    部门树

---
+++ crm-service/src/main/java/com/yy/crm/service/service/SysTreeService.java
@@ -0,0 +1,13 @@
+package com.yy.crm.service.service;
+
+import com.yy.crm.service.dto.DeptLevelDto;
+
+import java.util.List;
+
+/**
+ * @author 鲁源源 on 2018/1/11.
+ */
+public interface SysTreeService {
+
+    List<DeptLevelDto> deptTree();
+}
