b73ea805588a5d824360eb75e6ad5968831edf68
luyuanyuan <694436921@qq.com>
2018-01-25 22:24:40 +0800

    提交

--- crm-security/src/main/java/com/yy/crm/security/app/social/impl/AppSocialAuthenticationFilterPostProcessor.java
+++ crm-security/src/main/java/com/yy/crm/security/app/social/impl/AppSocialAuthenticationFilterPostProcessor.java
@@ -3,7 +3,6 @@ package com.yy.crm.security.app.social.impl;
 
 import com.yy.crm.security.core.social.SocialAuthenticationFilterPostProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.social.security.SocialAuthenticationFilter;
 import org.springframework.stereotype.Component;
@@ -17,12 +16,9 @@ public class AppSocialAuthenticationFilterPostProcessor implements SocialAuthent
 
     @Autowired
     private AuthenticationSuccessHandler myAuthenticationSuccessHandler;
-    @Autowired
-    private AuthenticationFailureHandler myAuthenticationFailureHandler;
 
     @Override
     public void process(SocialAuthenticationFilter socialAuthenticationFilter) {
         socialAuthenticationFilter.setAuthenticationSuccessHandler(myAuthenticationSuccessHandler);
-        socialAuthenticationFilter.setAuthenticationFailureHandler(myAuthenticationFailureHandler);
     }
 }
