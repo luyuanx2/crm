39c822f9168913301de0de04053bb1edb294baa5
luyuanyuan <694436921@qq.com>
2018-03-02 20:37:48 +0800

    提交

--- crm-security/src/main/java/com/yy/crm/security/core/SecurityCoreConfig.java
+++ crm-security/src/main/java/com/yy/crm/security/core/SecurityCoreConfig.java
@@ -2,10 +2,12 @@ package com.yy.crm.security.core;
 
 import com.yy.crm.security.core.properties.SecurityProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.expression.OAuth2WebSecurityExpressionHandler;
 
 /**
  * @author luyunayuan
@@ -20,10 +22,10 @@ public class SecurityCoreConfig {
         return new BCryptPasswordEncoder();
     }
 
-    //@Bean
-    //public OAuth2WebSecurityExpressionHandler oAuth2WebSecurityExpressionHandler(ApplicationContext applicationContext) {
-    //    OAuth2WebSecurityExpressionHandler expressionHandler = new OAuth2WebSecurityExpressionHandler();
-    //    expressionHandler.setApplicationContext(applicationContext);
-    //    return expressionHandler;
-    //}
+    @Bean
+    public OAuth2WebSecurityExpressionHandler oAuth2WebSecurityExpressionHandler(ApplicationContext applicationContext) {
+        OAuth2WebSecurityExpressionHandler expressionHandler = new OAuth2WebSecurityExpressionHandler();
+        expressionHandler.setApplicationContext(applicationContext);
+        return expressionHandler;
+    }
 }
