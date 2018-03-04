663c902b7dca7b335ecf608b6d30a236276f4241
luyuanyuan <694436921@qq.com>
2018-03-04 16:50:07 +0800

    新访客

---
+++ crm-service/src/main/java/com/yy/crm/service/mapper/SysVisitorMapper.java
@@ -0,0 +1,7 @@
+package com.yy.crm.service.mapper;
+
+import com.yy.crm.service.common.MyMapper;
+import com.yy.crm.service.model.SysVisitor;
+
+public interface SysVisitorMapper extends MyMapper<SysVisitor> {
+}
