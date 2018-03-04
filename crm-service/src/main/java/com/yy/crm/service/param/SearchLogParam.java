fff18bb34fc7140c43a32119b59dc4d7e0300298
luyuanyuan <694436921@qq.com>
2018-03-04 21:57:51 +0800

    时间转换处理

--- crm-service/src/main/java/com/yy/crm/service/param/SearchLogParam.java
+++ crm-service/src/main/java/com/yy/crm/service/param/SearchLogParam.java
@@ -1,9 +1,10 @@
 package com.yy.crm.service.param;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -23,9 +24,9 @@ public class SearchLogParam {
 
     private String operator;
 
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime beginTime;//yyyy-MM-dd HH:mm:ss
 
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime endTime;
 }
